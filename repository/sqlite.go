0a30af420a5824da72fb26c202a959bf66979937
Denis Gladkikh <denis@gladkikh.email>
2023-05-19 10:21:33 -0400

    Fix a few issues. Make sure to use the db location parameter

--- repository/sqlite.go
+++ repository/sqlite.go
@@ -2,34 +2,13 @@ package repository
 
 import (
 	"database/sql"
-	"fmt"
+	"github.com/Zmey56/openai-api-proxy/log"
 	_ "github.com/mattn/go-sqlite3"
-	"log"
-	"os"
 )
 
 // CreatedTableUsers create new table for users TO DO add colums with token and amount money
-func CreatedTableUsers() {
-	currentWorkingDirectory, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	pathDB := fmt.Sprintf("%s/openaiapiproxi.db", currentWorkingDirectory)
-
-	db, err := sql.Open("sqlite3", pathDB)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	log.Println(db)
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
+func CreatedTableUsers(db *sql.DB) {
+	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS users (
                         id INTEGER PRIMARY KEY,
                         login NOT NULL UNIQUE,
                         first_name TEXT NOT NULL,
@@ -48,7 +27,7 @@ func CreatedTableUsers() {
 		panic(err)
 	}
 
-	log.Println("Table for users have created!!!")
+	log.Debug.Println("table users has been created")
 
 }
 
